ef8fce55015796959d5bbcf77847559224b620db
Logan Farmer <logan@Logans-MacBook-Pro.local>
2016-02-29 11:23:16 -0600

    i had to fix the do while functions, forgot to add =

--- do_while.php
+++ do_while.php
@@ -4,15 +4,15 @@ $a = 0;
 $b = 2;
 do { 
 	echo "$a\n";
-	$a + 2;
-} while ($a <= 100)
+	$a += 2;
+} while ($a <= 100);
 
 do {
 	echo "$a\n";
-	$a -5;
-} while ($a >= -15)
+	$a -= 5;
+} while ($a >= -10);
 
 do {
 	echo "$b\n";
-	$b * $b
-} while ($b < 1000000)
+	$b = $b * $b;
+} while ($b < 1000000);
