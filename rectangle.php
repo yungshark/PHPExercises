9291d471b2d7ef1e7525a3c741b5a5f0459e4992
Logan Farmer <logan@Logans-MacBook-Pro.local>
2016-03-22 14:33:02 -0500

    rectangles exercise to be used with "shapes_test.php"

---
+++ rectangle.php
@@ -0,0 +1,24 @@
+<?php
+
+	class Rectangle
+	{
+		public $height;
+		public $width;
+
+		public function _contruct($height, $width){
+			$this-> = $height;
+			$this-> = $width;
+		}
+
+		public function area($height, width){
+			return $this->getHeight(); * $this->getWidth();
+		}
+
+		public function getHeight(){
+			return $this->height;
+		}
+		public function getWidth(){
+			return $this->width;
+		}
+
+	}
